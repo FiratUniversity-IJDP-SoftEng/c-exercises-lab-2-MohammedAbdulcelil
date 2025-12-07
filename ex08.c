7d19617becd3cfea8157bc058b0fbd14a12ed58d
MohammedAbdulcelil <mohammed536460@gmail.com>
2025-12-07 19:29:29 +0300

    Add functionality to calculate sum and average

--- ex08.c
+++ ex08.c
@@ -1,1 +1,17 @@
+const numbers = [];
+let sum = 0;
 
+
+for (let i = 0; i < 10; i++) {
+  const num = Number(prompt("Enter number " + (i + 1) + ":"));
+  numbers.push(num);
+  sum += num;
+}
+
+
+const average = sum / numbers.length;
+
+
+console.log("Numbers entered: " + numbers.join(", "));
+console.log("Sum: " + sum);
+console.log("Average: " + average);
