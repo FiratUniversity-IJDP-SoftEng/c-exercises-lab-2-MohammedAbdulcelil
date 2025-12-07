93bb89cf64bdd9ee66bfd0d79f0418af070b2201
MohammedAbdulcelil <mohammed536460@gmail.com>
2025-12-07 19:31:16 +0300

    Add number input and statistics calculation

--- ex09.c
+++ ex09.c
@@ -1,1 +1,19 @@
+let numbers = [];
+let input;
 
+
+while (true) {
+  input = Number(prompt("Enter a number (-1 to stop):"));
+
+  if (input === -1) {
+    break;  
+  }
+  numbers.push(input);
+}
+let count = numbers.length;
+let sum = numbers.reduce((a, b) => a + b, 0);
+let average = count > 0 ? sum / count : 0;
+
+console.log("Count of numbers entered: " + count);
+console.log("Sum of numbers: " + sum);
+console.log("Average of numbers: " + average);
