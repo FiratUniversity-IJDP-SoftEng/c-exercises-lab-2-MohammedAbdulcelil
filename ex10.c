00e50193d1cd231a0e38770bf2d55efff1f7ee85
MohammedAbdulcelil <mohammed536460@gmail.com>
2025-12-07 19:32:36 +0300

    Add multiplication table output in ex10.c

--- ex10.c
+++ ex10.c
@@ -1,1 +1,9 @@
+for (let row = 1; row <= 10; row++) {
+  let line = "";
+  
+  for (let col = 1; col <= 10; col++) {
+    line += (row * col).toString().padStart(4, " ");
+  }
 
+  console.log(line);
+}
