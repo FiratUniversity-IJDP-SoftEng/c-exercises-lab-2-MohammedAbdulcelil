057f371125844757dbe41987d3d93576492787e7
MohammedAbdulcelil <mohammed536460@gmail.com>
2025-12-07 19:46:06 +0300

    Convert JavaScript logic to C for number printing
    Replaced JavaScript code with C code to print numbers.

--- ex06.c
+++ ex06.c
@@ -1,8 +1,22 @@
 
-const result = [];
+#include <stdio.h>
 
-for (let i = 99; i >= 50; i -= 3) {
-  result.push(i);
+int main() {
+    int numbers[20]; 
+    int index = 0;
+
+
+    for (int i = 99; i >= 50; i -= 3) {
+        numbers[index++] = i;
+    }
+
+
+    for (int i = 0; i < index; i++) {
+        printf("%d", numbers[i]);
+        if (i < index - 1) {
+            printf(", ");
+        }
+    }
+
+    return 0;
 }
-
-console.log(result);
