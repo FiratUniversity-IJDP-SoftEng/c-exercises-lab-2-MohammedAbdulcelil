aba02091a90f70427d2d54bad26fc956e71b1e41
MohammedAbdulcelil <mohammed536460@gmail.com>
2025-12-07 19:56:46 +0300

    Implement X pattern printing in ex11.c
    Add a program to print an X pattern using asterisks

--- ex11.c
+++ ex11.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
 
+int main() {
+    int n = 5;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+        
+            if (i == j || j == n - i - 1) {
+                printf("*");
+            } else {
+                printf(" ");
+            }
+        }
+        printf("\n"); 
+    }
+    return 0;
+}
